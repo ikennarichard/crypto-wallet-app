1265b0137a276a3207c9db225f876a17fecd94bb
Ikenna Richard <oguejioforrichard@gmail.com>
2025-11-09 20:28:59 +0100

    feat: add gluestack ui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,15 @@
 import { Stack } from "expo-router";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import "@/global.css";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
   return (
-    <Stack initialRouteName="(tabs)" screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(tabs)" />
-    </Stack>
+    <GluestackUIProvider mode="light">
+      <StatusBar />
+      <Stack initialRouteName="(tabs)" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" />
+      </Stack>
+    </GluestackUIProvider>
   );
 }
